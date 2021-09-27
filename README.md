c848538741b8f49e220101d1053a8d92252d7ec4
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2021-09-27 20:30:02 +0300

    Transform to PiZero

--- README.md
+++ README.md
@@ -18,19 +18,11 @@
 #### Поля:
 * __alive - rospy.ServiceProxy: gs_interfaces.srv.Live
 * __restart - rospy.ServiceProxy: std_srvs.srv.Empty
-* __time_service - rospy.ServiceProxy: gs_interfaces.srv.Time
-* __uptime_service - rospy.ServiceProxy: gs_interfaces.srv.Time
-* __flight_time_service - rospy.ServiceProxy: gs_interfaces.srv.Time
-* __info_service - rospy.ServiceProxy: gs_interfaces.srv.Info
 * __get_parameters_service - rospy.ServiceProxy: gs_interfaces.srv.ParametersList
 * __set_parameters_service - rospy.ServiceProxy: gs_interfaces.srv.SetParametersList
 
 #### Методы:
 * runStatus - возвращает статус подключения RPi к базовой плате Пионер
-* boardNumber - возвращает имя/номер платы
-* time - возвращает время с момента включения коптера
-* uptime - возвращает время запуска для системы навигации
-* flightTime - возвращает время с начала полета
 * restart - перезапуск базовой платы Пионер
 * getParameters - возвращает параметры АП
 * setParameters(params_dict) - устанавливает параметры АП, params_dict - словарь параметров (название_параметра:значение_параметра)
@@ -38,10 +30,6 @@
 #### Используемые сервисы:
 * geoscan/alive (gs_interfaces/Live)
 * geoscan/board/restart (std_srvs/Empty)
-* geoscan/board/get_info (gs_interfaces/Info)
-* geoscan/board/get_time (gs_interfaces/Time)
-* geoscan/board/get_uptime (gs_interfaces/Time)
-* geoscan/board/get_flight_time (gs_interfaces/Time)
 * geoscan/board/get_parameters (gs_interfaces/ParametersList)
 * geoscan/board/set_parameters (gs_interfaces/SetParametersList) 
 
--- gs_board/core.py
+++ gs_board/core.py
@@ -2,59 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from gs_interfaces.srv import Time, Info, Live, SetParametersList, ParametersList
-from gs_interfaces.msg import SimpleBatteryState, Parameter
+from gs_interfaces.srv import Live, SetParametersList, ParametersList
+from gs_interfaces.msg import Parameter
 from rospy import ServiceProxy
-from std_msgs.msg import Bool
 from std_srvs.srv import Empty
 
 class BoardManager():
 
-    def __init__(self):
-        rospy.wait_for_service("geoscan/alive")
-        rospy.wait_for_service("geoscan/board/restart")
-        rospy.wait_for_service("geoscan/board/get_time")
-        rospy.wait_for_service("geoscan/board/get_uptime")
-        rospy.wait_for_service("geoscan/board/get_flight_time")
-        rospy.wait_for_service("geoscan/board/get_info")
-        rospy.wait_for_service("geoscan/board/get_parameters")
-        rospy.wait_for_service("geoscan/board/set_parameters")
+    def __init__(self, namespace = ""):
+        if namespace != "":
+            namespace += "/"
+        rospy.wait_for_service(f"{namespace}geoscan/alive")
+        rospy.wait_for_service(f"{namespace}geoscan/board/restart")
+        rospy.wait_for_service(f"{namespace}geoscan/board/get_parameters")
+        rospy.wait_for_service(f"{namespace}geoscan/board/set_parameters")
 
-        self.__alive = ServiceProxy("geoscan/alive",Live)
-        self.__restart = ServiceProxy("geoscan/board/restart", Empty)
-        self.__time_service = ServiceProxy("geoscan/board/get_time",Time)
-        self.__uptime_service = ServiceProxy("geoscan/board/get_uptime",Time)
-        self.__flight_time_service = ServiceProxy("geoscan/board/get_flight_time",Time)
-        self.__info_service = ServiceProxy("geoscan/board/get_info",Info)
-        self.__get_parameters_service = ServiceProxy("geoscan/board/get_parameters", ParametersList)
-        self.__set_parameters_service = ServiceProxy("geoscan/board/set_parameters", SetParametersList)
+        self.__alive = ServiceProxy(f"{namespace}geoscan/alive", Live)
+        self.__restart = ServiceProxy(f"{namespace}geoscan/board/restart", Empty)
+        self.__get_parameters_service = ServiceProxy(f"{namespace}geoscan/board/get_parameters", ParametersList)
+        self.__set_parameters_service = ServiceProxy(f"{namespace}geoscan/board/set_parameters", SetParametersList)
     
     def runStatus(self):
         return self.__alive().status
-
-    def boardNumber(self):
-        if self.runStatus():
-            return self.__info_service().num
-        else:
-            rospy.logwarn("Wait, connecting to flight controller")
-
-    def time(self):
-        if self.runStatus():
-            return self.__time_service().time
-        else:
-            rospy.logwarn("Wait, connecting to flight controller")
-
-    def uptime(self):
-        if self.runStatus():
-            return self.__uptime_service().time
-        else:
-            rospy.logwarn("Wait, connecting to flight controller")
-
-    def flightTime(self):
-        if self.runStatus():
-            return self.__flight_time_service().time
-        else:
-            rospy.logwarn("Wait, connecting to flight controller")
 
     def restart(self):
         self.__restart()
